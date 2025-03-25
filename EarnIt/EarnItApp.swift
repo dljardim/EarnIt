64ba64c6f919c90cb031b4d5071612b8427e4f0d
dljardim <dljardim@outlook.com>
2025-03-25 09:30:11 -0500

    Initial Commit

---
+++ EarnIt/EarnItApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarnItApp.swift
+//  EarnIt
+//
+//  Created by Damian Jardim on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct EarnItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
